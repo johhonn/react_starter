7d7d769b755bc86b443e5a5bedc495cbdb8c64e5
johhonn <35551051+johhonn@users.noreply.github.com>
2022-01-09 20:27:33 -0500

    Initialize develop

---
+++ packages/vite-app-ts/src/index.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+/**
+ * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
+ * See MainPage.tsx for main app component!
+ * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
+ *
+ * This file loads react.
+ * You don't need to change this file.
+ */
+
+/**
+ * Loads {@see App} which sets up the application async.
+ * The main page is in the component {@see MainPage}
+ */
+const run = async (): Promise<void> => {
+  // dynamic imports for code splitting
+  const { lazy, Suspense, StrictMode } = await import('react');
+  const ReactDOM = await import('react-dom');
+  await import('./helpers/__global');
+  const App = lazy(() => import('./App'));
+
+  ReactDOM.render(
+    <StrictMode>
+      <Suspense fallback={<div />}>
+        <App />
+      </Suspense>
+    </StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+void run();
+
+export {};
